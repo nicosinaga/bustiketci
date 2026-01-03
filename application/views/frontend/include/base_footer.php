f23701498339deaf3131e3b98f420db6c77cd288
nicosinaga <nsinaga260@gmail.com>
2026-01-03 14:32:10 +0700

    first commit

---
+++ application/views/frontend/include/base_footer.php
@@ -0,0 +1,65 @@
+			<footer class="footer-area section-gap">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-3  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4 class="text-white">Pemesanan Tiket Bus</h4>
+								<p>
+								Tiket BUS adalah layanan pemesanan tiket bus online terbesar di dunia. Dipercaya oleh lebih dari 8 juta pelanggan secara global. Bus Tickets menawarkan pemesanan tiket bus melalui website.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4 class="text-white">Contact Us</h4>
+								<p>
+
+								</p>
+								<p class="number">
+									Framework ID <br>
+								</p>
+							</div>
+						</div>						
+						<div class="col-lg-5  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4 class="text-white">Newsletter</h4>
+								<p>You can trust us. we only send  offers, not a single spam.</p>
+								<div class="d-flex flex-row" id="mc_embed_signup">
+										<form class="navbar-form" novalidate="true" action="" method="post">
+									    <div class="input-group add-on">
+									      	<input class="form-control" placeholder="Email address"  type="email">
+											<div style="position: absolute; left: -5000px;">
+												<input name="" tabindex="-1" value="" type="text">
+											</div>
+									      <div class="input-group-btn">
+									        <button class="genric-btn primary circle arrow"><span class="lnr lnr-arrow-right"></span></button>
+									      </div>
+									    </div>
+									      <div class="info mt-20"></div>									    
+									  </form>
+
+								</div>
+							</div>
+						</div>						
+					</div>
+					<div class="footer-bottom d-flex justify-content-between align-items-center flex-wrap">
+						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            <p class="footer-text m-0"><span>&copy; <?= date('Y') ?> Sistem Pemesanan Tiket Bus </span> </p>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+			<!-- Log on to codeastro.com for more projects -->
+						<!-- <div class="footer-social d-flex align-items-center">
+							<a href="#"><i class="fas fa-facebook"></i></a>
+							<a href="#"><i class="fas fa-twitter"></i></a>
+							<a href="#"><i class="fas fa-dribbble"></i></a>
+							<a href="#"><i class="fas fa-behance"></i></a>
+						</div> -->
+					</div>
+				</div>
+			</footer>
+			<div class="preloader">
+			<div class="loading">
+				<img src="<?php echo base_url('assets/frontend/img/preloader.gif') ?>" width="100">
+				<p>Please Wait...</p>
+			</div>
+		</div>	
+			<!-- End footer Area -->
